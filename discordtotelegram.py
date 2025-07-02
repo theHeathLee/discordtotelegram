9575caef0172f13218db40ff368bff776ff9daee
Heath Lee <theHeathLee@users.noreply.github.com>
2025-07-02 02:49:05 +0200

    Initial commit (reset history)

---
+++ discordtotelegram.py
@@ -0,0 +1,27 @@
+import discord
+import requests
+import os
+
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
+TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+TELEGRAM_CHAT_ID = os.environ['TELEGRAM_CHAT_ID']
+
+
+intents = discord.Intents.default()
+intents.voice_states = True
+intents.guilds = True
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_voice_state_update(member, before, after):
+    if before.channel is None and after.channel is not None:
+        message = f"{member.display_name} joined voice channel: {after.channel.name}"
+        url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+        requests.post(url, data={
+            "chat_id": TELEGRAM_CHAT_ID,
+            "text": message
+        })
+
+client.run(DISCORD_TOKEN)
