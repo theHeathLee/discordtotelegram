e51ae92911472c735b7a787b69abbef4e1ec1798
Heath Lee <theHeathLee@users.noreply.github.com>
2025-10-08 22:14:35 +0200

    add special messages

--- discordtotelegram.py
+++ discordtotelegram.py
@@ -18,11 +18,23 @@ client = discord.Client(intents=intents)
 async def on_voice_state_update(member, before, after):
     if before.channel is None and after.channel is not None:
         if after.channel.name != "Secret Coup":
-            if member.display_name != "Hunterly":
+            if member.display_name == "GEEK Music":
                 #do not send message for GEEK Music
                 return
+            elif member.display_name == "Hunterly":
+                message = f"Hunters here, time to get this bread"
+            elif member.display_name == "SPKross":
+                message = f"Jacob the absolute legend has arrived"
+            elif member.display_name == "Heath":
+                message = f"All Rise for King Heath"
+            elif member.display_name == "Currahee901":
+                message = f"God help us, Tommy has entered the building"
+            elif member.display_name == "Futsch":
+                message = f"Selim wants to hunt some ghosts"
+            elif member.display_name == "Jamirous":
+                message = f"Jan is here to loot in the blue zone"
             else:
-                message = f"{member.display_name} the bitch just joined {after.channel.name}"
+                message = f"{member.display_name} the just joined {after.channel.name}"
             url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
             requests.post(url, data={
                 "chat_id": TELEGRAM_CHAT_ID,
